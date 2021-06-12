920dfa44efc2d3033ed12ff17c57adaee98158ac
iagomussel <him_mussel@live.com>
2021-06-12 19:15:50 -0300

    editor

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,43 +1,42 @@
 <template>
-<div>
-  <NavBar title="Larodon"/>
+  <div>
+    <NavBar title="Larodon" />
     <div class="progress">
       <div class="determinate" style="width: 0"></div>
-  </div>
+    </div>
     <div class="container section">
-            <router-view />
+      <router-view />
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar"
-import M from 'materialize-css'
-import webClient from './client_axios.js';
+import NavBar from "./components/NavBar";
+import M from "materialize-css";
+import webClient from "./client_axios.js";
 
-setTimeout(refresh_token,15000);
-function refresh_token(){
-  webClient.get('/refreshtoken').then(res=>{
-    localStorage.setItem("user", JSON.stringify(res.data));
-  }).catch(() =>{
-    location.reload();
-  })
-  setTimeout(refresh_token,15000);
+setTimeout(refresh_token, 15000);
+function refresh_token() {
+  webClient
+    .get("/refreshtoken")
+    .then((res) => {
+      localStorage.setItem("user", JSON.stringify(res.data));
+    })
+    .catch(() => {
+      location.reload();
+    });
+  setTimeout(refresh_token, 15000);
 }
 let App = {
-  name: 'App',
-}
-App.components={NavBar};
-App.watch={
-  $route (){
+  name: "App",
+};
+App.components = { NavBar };
+App.watch = {
+  $route() {
     M.AutoInit();
-  }
-}
-export default App
+  },
+};
+export default App;
 </script>
 
-
-
-<style>
-
-</style>
+<style></style>
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pm2 link $PM2_PUBLIC_KEY $PM2_SECRET_KEY
-npm run pm2 --prefix app          #-- app
+npm run pm2 --prefix app  #-- app
 npm run pm2 --prefix api  #-- api
 cd api
 sequelize-cli db:migrate
